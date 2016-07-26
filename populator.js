a9ca5026bb003858a06cc789c5342097a5183a3f
Paulo Diovani <paulo@diovani.com>
2016-07-26 13:05:04 -0300

    Fix files accordind to linter

--- populator.js
+++ populator.js
@@ -1,28 +1,30 @@
+'use strict';
+
 const Firebase = require('firebase');
-const faker = require('faker');
+const Faker = require('faker');
 
 const ref = Firebase.initializeApp({
-  databaseURL: 'https://is-app-3b28f.firebaseio.com/',
-  serviceAccount: './serviceAccountCredentials.json'
+    databaseURL: 'https://is-app-3b28f.firebaseio.com/',
+    serviceAccount: './serviceAccountCredentials.json'
 }).database().ref('queue');
 
 const operations = [];
-var counter = 0;
+let counter = 0;
 
-while(counter < 1000) {
+while (counter < 1000) {
 
-  operations.push(
-    ref
-      .child('tasks')
-      .push({
-        name: faker.name.findName(),
-        number: ++counter
-      })
+    operations.push(
+        ref
+        .child('tasks')
+        .push({
+            name: Faker.name.findName(),
+            number: ++counter
+        })
     );
 }
 
 Promise.all(operations).then(() => {
 
-  console.log('Done');
-  process.exit();
+    console.log('Done');
+    process.exit();
 });
--- worker.js
+++ worker.js
@@ -1,15 +1,20 @@
+'use strict';
+
 const Firebase = require('firebase');
 const Queue = require('firebase-queue');
 
 const ref = Firebase.initializeApp({
-  databaseURL: 'https://is-app-3b28f.firebaseio.com/',
-  serviceAccount: './serviceAccountCredentials.json'
+    databaseURL: 'https://is-app-3b28f.firebaseio.com/',
+    serviceAccount: './serviceAccountCredentials.json'
 }).database().ref('queue');
 
-const queue = new Queue(ref, { numWorkers: 2 }, (data, progress, resolve, reject) => {
-  console.log(data);
-  if (data.number === 171) {
-    reject('deu ruim');
-  }
-  resolve();
+new Queue(ref, { numWorkers: 2 }, (data, progress, resolve, reject) => {
+
+    console.log(data);
+
+    if (data.number === 171) {
+        reject('deu ruim');
+    }
+
+    resolve();
 });
